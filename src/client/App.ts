bd617fd099a04c7ab5ca5ef9cd83d13c68c56a3e
TongTwist <arnaud.dussart@gmail.com>
2022-04-15 12:00:40 +0200

    rebase with main

--- src/client/App.ts
+++ src/client/App.ts
@@ -1,5 +1,7 @@
 import * as THREE from "three";
 import { GUI } from "dat.gui";
+import Stats from "three/examples/jsm/libs/stats.module";
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
 export class App {
   speedFactor = 1;
@@ -12,7 +14,9 @@ export class App {
     private readonly _renderer: THREE.WebGLRenderer,
     private readonly _scene: THREE.Scene,
     private readonly _camera: THREE.PerspectiveCamera,
-    private readonly _gui: GUI
+    private readonly _gui: GUI,
+    private readonly _stats: Stats,
+    private readonly _orbitControls: OrbitControls,
   ) {}
 
   get width(): number {
@@ -50,18 +54,23 @@ export class App {
   frameAnimation(t: number) {
     requestAnimationFrame(this.frameAnimation.bind(this));
     this.render();
+    this._stats.update();
   }
 
   static create(): App {
     const renderer = new THREE.WebGLRenderer();
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(55, App._ratio(), 0.1, 100);
+    
+    const stats = Stats();
+    const orbitControls = new OrbitControls(camera , renderer.domElement);
 
     const gui = new GUI();
-    const res = new App(renderer, scene, camera, gui);
+    const res = new App(renderer, scene, camera, gui, stats, orbitControls);
     res.onWindowResize();
 
     document.body.appendChild(renderer.domElement);
+    document.body.appendChild(stats.dom);
 
     const appParam = gui.addFolder("App properties");
     appParam.open();
