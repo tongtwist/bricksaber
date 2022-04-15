399a7a5627ea4829a7f3343fc3744091d2263dda
TongTwist <arnaud.dussart@gmail.com>
2022-04-15 10:02:09 +0200

    renderLoop

--- src/client/App.ts
+++ src/client/App.ts
@@ -7,19 +7,32 @@ export class App {
 	lightColor = 0xffffff
 
 	private constructor (
+		private readonly _renderer: THREE.WebGLRenderer,
 		private readonly _scene: THREE.Scene,
+		private readonly _camera: THREE.PerspectiveCamera,
 		private readonly _gui: GUI
 	) {}
 
 	run() {
-		
+		this.frameAnimation(0)
+	}
+
+	render () {
+		this._renderer.render(this._scene, this._camera)
+	}
+
+	frameAnimation (t: number) {
+		requestAnimationFrame(this.frameAnimation.bind(this))
+		this.render()
 	}
 
 	static create(): App {
+		const renderer = new THREE.WebGLRenderer()
 		const scene = new THREE.Scene()
+		const camera = new THREE.PerspectiveCamera(55, 800 / 600, 0.1, 100)
 
 		const gui = new GUI()
-		const res = new App(scene, gui)
+		const res = new App(renderer, scene, camera, gui)
 
 		const appParam = gui.addFolder("App properties")
 		appParam.open()
