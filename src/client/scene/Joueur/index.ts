0486c10eaaa7b8687dfbef51e6c4799506c17b2b
aissatou <72610380+aissou20@users.noreply.github.com>
2022-04-15 15:23:49 +0200

    ajout du joueur

---
+++ src/client/scene/Joueur/index.ts
@@ -0,0 +1,13 @@
+// reation classe joueur + prop sabre x2 
+//new de class sabre
+
+import * as THREE from "three";
+import { Sabre } from "./Sabre";
+
+export class Joueur extends Sabre {
+
+    public constructor(){
+        super();
+    }
+
+}
