b34dfbef07e2abac007e8232354a1c14be597de7
Tristan Defachel <defachel.tristan@gmail.com>
2022-04-15 10:33:32 +0200

    feat(added resizing canvas)

--- src/client/App.ts
+++ src/client/App.ts
@@ -1,44 +1,55 @@
-import * as THREE from "three"
-import { GUI } from "dat.gui"
-
+import * as THREE from "three";
+import { GUI } from "dat.gui";
 
 export class App {
-	speedFactor = 1
-	lightColor = 0xffffff
+  speedFactor = 1;
+  lightColor = 0xffffff;
+  width = window.innerWidth;
+  heigth = Math.max(window.innerHeight, 1);
 
-	private constructor (
-		private readonly _renderer: THREE.WebGLRenderer,
-		private readonly _scene: THREE.Scene,
-		private readonly _camera: THREE.PerspectiveCamera,
-		private readonly _gui: GUI
-	) {}
+  private constructor(
+    private readonly _renderer: THREE.WebGLRenderer,
+    private readonly _scene: THREE.Scene,
+    private readonly _camera: THREE.PerspectiveCamera,
+    private readonly _gui: GUI
+  ) {}
 
-	run() {
-		this.frameAnimation(0)
-	}
+  run() {
+    this.frameAnimation(0);
+  }
 
-	render () {
-		this._renderer.render(this._scene, this._camera)
-	}
+  render() {
+    this._renderer.render(this._scene, this._camera);
+  }
 
-	frameAnimation (t: number) {
-		requestAnimationFrame(this.frameAnimation.bind(this))
-		this.render()
-	}
+  frameAnimation(t: number) {
+    requestAnimationFrame(this.frameAnimation.bind(this));
+    this.render();
+  }
 
-	static create(): App {
-		const renderer = new THREE.WebGLRenderer()
-		const scene = new THREE.Scene()
-		const camera = new THREE.PerspectiveCamera(55, 800 / 600, 0.1, 100)
+  create(): App {
+    const renderer = new THREE.WebGLRenderer();
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(55, this._ratio(), 0.1, 100);
 
-		const gui = new GUI()
-		const res = new App(renderer, scene, camera, gui)
+    const gui = new GUI();
+    const res = new App(renderer, scene, camera, gui);
 
-		document.body.appendChild(renderer.domElement)
+    document.body.appendChild(renderer.domElement);
 
-		const appParam = gui.addFolder("App properties")
-		appParam.open()
-		
-		return res
-	}
+    const appParam = gui.addFolder("App properties");
+    appParam.open();
+
+    return res;
+  }
+
+  private onWindowResize() {
+    this._camera.aspect = this._ratio();
+    this._camera.updateProjectionMatrix();
+    this._renderer.setSize(this.width, this.heigth);
+  }
+
+  private _ratio() {
+    return this.width / this.heigth;
+  }
 }
