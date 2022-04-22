8215d45cd19e8bfa09840315bbf4fda3b6265d76
lison.dagostini <lison.dagostini@gmail.com>
2022-04-22 17:19:07 +0200

    color square

--- src/client/Scene/Decor/OutsideDecorSquare.ts
+++ src/client/Scene/Decor/OutsideDecorSquare.ts
@@ -2,9 +2,9 @@ import type { GUIContainer } from "../../Components";
 import { Cylinder } from "../../Templates/Cylinder";
 
 export default class OutsideDecorSquare extends Cylinder {
-  constructor(parentGUIContainer: GUIContainer,name:string,number:number) {
+  constructor(parentGUIContainer: GUIContainer, name: string, number: number) {
     super({
-      name: "Outside Decor Square "+name,
+      name: "Outside Decor Square " + name,
       transparent: true,
       opacity: 1,
       radiusTop: 8,
@@ -13,11 +13,10 @@ export default class OutsideDecorSquare extends Cylinder {
       radialSegments: 4,
       heightSegments: 4,
       openEnded: true,
-      color: 0x0000ff,
+      color: 0xa4f4fa,
       gui: { container: parentGUIContainer },
     });
     this._obj3D.rotation.x = -Math.PI * 0.5;
     this._obj3D.position.z = number * 10;
-    this._obj3D.rotation.y= number;
   }
 }
