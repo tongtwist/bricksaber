cc8143e46a1cb9a2d53d3809f3d5349a0c42c304
agence-web-france <mgelle.agencewebfrance@gmail.com>
2022-04-22 11:11:52 +0200

    wip: add edges to player platform

--- src/client/Scene/Decor/PlayerPlatform.ts
+++ src/client/Scene/Decor/PlayerPlatform.ts
@@ -6,9 +6,9 @@ import { Platform } from "../../Templates"
 export default class PlayerPlatform extends Platform {
 	constructor(parentGUIContainer: GUIContainer) {
 		super({
-			name: "Platform1",
+			name: "PlayerPlatform",
 			transparent: true,
-			opacity: .8,
+			opacity: 0.5,
 			width: 2,
 			height: 2,
 			length: 0.25,
@@ -17,5 +17,8 @@ export default class PlayerPlatform extends Platform {
 		})
 		this._obj3D.rotation.x = -Math.PI * .5
 		this._obj3D.position.y = 0
+		const edges = new EdgesGeometry(this._obj3D.geometry)
+		const line = new LineSegments(edges, new LineBasicMaterial({ color: 0xffffff }));
+		this._obj3D.add(line)
 	}
 }
